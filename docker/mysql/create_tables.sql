1885ed962a883376ccb5d7d0c6a262c737ac4a60
Jack <skrok.jacek21@gmail.com>
2024-07-17 22:36:21 +0200

    Refactor database building

--- docker/mysql/create_tables.sql
+++ docker/mysql/create_tables.sql
@@ -1,24 +1,24 @@
 CREATE TABLE cars (
-    car_id INT AUTO_INCREMENT PRIMARY KEY,
+    car_id SMALLINT AUTO_INCREMENT PRIMARY KEY,
     car_brand VARCHAR(80) NOT NULL,
     car_model VARCHAR(80) NOT NULL,
     car_license_plate VARCHAR(20) UNIQUE NOT NULL,
     car_description VARCHAR(1024),
-    status ENUM('available', 'in_use', 'maintenance') NOT NULL,
-    occupied_from DATE,
-    occupied_to DATE
+    car_status ENUM('available', 'in_use', 'maintenance') NOT NULL,
+    occupied_from DATETIME(6),
+    occupied_to DATETIME(6)
 );
 
 CREATE TABLE drivers (
-    driver_id INT AUTO_INCREMENT PRIMARY KEY,
+    driver_id SMALLINT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     driver_department VARCHAR(80)
 );
 
 CREATE TABLE car_drivers (
-    car_id INT NOT NULL,
-    driver_id INT NOT NULL,
+    car_id SMALLINT NOT NULL,
+    driver_id SMALLINT NOT NULL,
     PRIMARY KEY (car_id, driver_id),
     FOREIGN KEY (car_id) REFERENCES cars(car_id),
     FOREIGN KEY (driver_id) REFERENCES drivers(driver_id)
--- docker/mysql/insert_data.sql
+++ docker/mysql/insert_data.sql
@@ -1,5 +1,5 @@
 -- Insert data into the cars table
-INSERT INTO cars (car_brand, car_model, car_license_plate, car_description, status, occupied_from, occupied_to)
+INSERT INTO cars (car_brand, car_model, car_license_plate, car_description, car_status, occupied_from, occupied_to)
 VALUES
     ('Toyota', 'Camry', 'ABC123', 'Blue sedan', 'available', NULL, NULL),
     ('Honda', 'Civic', 'XYZ456', 'Red compact car', 'in_use', '2024-06-01', '2024-06-30'),
