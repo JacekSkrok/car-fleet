c2e156ce140148da0651b9ac7b7502896e5a0546
Jack <skrok.jacek21@gmail.com>
2024-08-07 15:43:10 +0200

    Add endpoint to return all drivers

--- src/main/java/com/lazyjack/resource/DriverResource.java
+++ src/main/java/com/lazyjack/resource/DriverResource.java
@@ -6,10 +6,10 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.Application;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import java.util.List;
 import java.util.Optional;
 
 @Path("/drivers")
@@ -17,6 +17,19 @@ public class DriverResource {
 
     @Inject
     DriverRepository driverRepository;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllDrivers() {
+        List<Driver> drivers = driverRepository.listAll();
+        if( !drivers.isEmpty()) {
+            return Response.ok(drivers).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("There are no drivers")
+                    .build();
+        }
+    }
 
     @GET
     @Path("/{driverId}")
